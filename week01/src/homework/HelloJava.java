cc118f52017d4e5261bfce0267a22e1891d177fe
K-eunhye <11233211@naver.com>
2024-11-25 09:17:12 +0900

    241125 1주차 주말과제

---
+++ week01/src/homework/HelloJava.java
@@ -0,0 +1,8 @@
+package homework;
+
+public class HelloJava {
+	public static void main(String[] args) { // 자바 프로그램이 실행되면 메인 메소드 코드가 실행된다
+		System.out.println("HelloJava"); // 출력메소드 System안에 옵션 out 안에 println을 사용하며 괄호()안에 출력하고자 하는 내용을 작성한다. 숫자가 아니라 문자라면 꼭 큰 따옴표를 써야 오류가 발생하지 않는다
+	}
+
+}
---
+++ week01/src/homework/PrintInfo2.java
@@ -0,0 +1,8 @@
+package homework;
+
+public class PrintInfo2 {
+	public static void main(String[] args) { // 자바 프로그램이 실행되면 메인 메소드 코드가 실행된다
+		System.out.print("2024년 11월 24일"); // 프린트라는 출력메소드 옵션을 쓰는데 기본 print()사용하면 줄바꿈 없이 내용 출력을 한다
+		System.out.print(" 00시 46분"); // 프린트라는 출력메소드 옵션을 쓰는데 기본 print()사용하면 줄바꿈 없이 내용 출력을 한다, 앞 칸과 글씨가 붙을까봐 띄어쓰기도 한칸 넣어주었다
+	}
+}
